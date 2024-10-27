c867c489759928a8e09b1f9077ddb3762f94cf47
taurine1000mg <greenpeace1410@gmail.com>
2024-10-27 09:58:10 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>課題007</title>
+</head>
+<body>
+  <p>
+      <?php
+      $items = ['name'=>'onion', 'price'=>'200', 'weight'=>'160'];
+
+      print_r($items);
+      ?>
+  </p>
+</body>
+</html>
